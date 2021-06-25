98327500438cd744720bfbe754a222ed362412ea
andarpratama <master.amarta@gmail.com>
2021-06-26 02:09:12 +0700

    install react-chat-engine

--- src/App.js
+++ src/App.js
@@ -1,2 +1,17 @@
-import { ChatEngine } from 'react-chat-engine'
+import { ChatEngine } from 'react-chat-engine';
 import './App.css'
+import ChatFeed from './components/ChatFeed';
+
+const App = () => {
+   return (
+      <ChatEngine
+         height="100vh"
+         projectID="3a752f5e-9228-4313-a3e3-3842d6de490e"
+         userName="andar"
+         userSecret="andar"
+         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      />
+   )
+}
+
+export default App
